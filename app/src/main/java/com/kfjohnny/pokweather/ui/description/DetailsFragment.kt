d9b893727300f17740d51784a9bcfb5cc3bd508e
kfjohnny2 <kfjohnny@ufrn.edu.br>
2021-02-24 20:15:43 +0000

    Changing background color dynamically by the pokemon dominant colors; Uppercase on pokemon name first character;

--- app/src/main/java/com/kfjohnny/pokweather/ui/description/DetailsFragment.kt
+++ app/src/main/java/com/kfjohnny/pokweather/ui/description/DetailsFragment.kt
@@ -1,16 +1,26 @@
 package com.kfjohnny.pokweather.ui.description
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
+import androidx.palette.graphics.Palette.Swatch
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.kfjohnny.pokweather.R
 import com.kfjohnny.pokweather.base.BaseFragment
 import com.kfjohnny.pokweather.databinding.FragmentDetailsBinding
 import com.kfjohnny.pokweather.ui.description.adapter.MovesAdapter
+import com.kfjohnny.pokweather.util.changeDynamicBackgroundColor
 import org.koin.android.viewmodel.ext.android.viewModel
+
 
 class DetailsFragment : BaseFragment<FragmentDetailsBinding>() {
 
@@ -31,10 +41,18 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding>() {
 
         binding.detailsViewModel = detailsViewModel
 
+        configureLiveData()
         configuraRecyclerView()
         return binding.root
     }
 
+    private fun configureLiveData(){
+        detailsViewModel.pokemonData.observe(viewLifecycleOwner, Observer {
+
+            //Changing background color dynamically by the pokemon dominant color
+            activity?.let { it1 -> changeDynamicBackgroundColor(it, it1, binding.cvBackground) }
+        })
+    }
     private fun configuraRecyclerView() {
         binding.rvMoves.adapter = MovesAdapter(mutableListOf())
         with(binding.rvMoves) {
