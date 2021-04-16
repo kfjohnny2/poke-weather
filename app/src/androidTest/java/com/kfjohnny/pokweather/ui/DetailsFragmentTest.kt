53eb3ed9bc3124b3e685f6a9fac96fad28d9c99d
kfjohnny2 <kfjohnny@ufrn.edu.br>
2021-04-16 03:14:54 +0100

    - Removed useless comments;

--- app/src/androidTest/java/com/kfjohnny/pokweather/ui/DetailsFragmentTest.kt
+++ app/src/androidTest/java/com/kfjohnny/pokweather/ui/DetailsFragmentTest.kt
@@ -20,7 +20,6 @@ import org.junit.runner.RunWith
 class DetailsFragmentTest {
     @Before
     fun setupFragment(){
-        // The "fragmentArgs" and "factory" arguments are optional.
         val fragmentArgs = Bundle(1).apply {
             putString("pokemonId", "2")
         }
